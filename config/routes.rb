66b257facfeed8cee404a3c5ce0300b71e5a8e25
dan <ulumadan@gmail.com>
2018-06-23 19:14:03 +0300

    CRUD create

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'robots/new'
+	resources :robots
+  # get 'robots/new'
 
-  get 'robots/create'
+  # get 'robots/create'
 
-  get 'robots/index'
+  # get 'robots/index'
 
-  get 'robots/show'
+  # get 'robots/show'
 
-  get 'robots/edit'
+  # get 'robots/edit'
 
-  get 'robots/update'
+  # get 'robots/update'
 
-  get 'robots/destroy'
+  # get 'robots/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
